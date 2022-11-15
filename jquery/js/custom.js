f51dad21e05e6437c17540e3477ec6995b2feb4e
Gabriel Soares <gabrsoares@outlook.com>
2022-11-15 16:10:11 -0300

    finalizando projeto

--- jquery/js/custom.js
+++ jquery/js/custom.js
@@ -7,6 +7,7 @@ $(document).ready(function(){
     let itens = $('.featured-item') //class
     let destaques = $('#featured') //id
 
+ 
     //Configuração de produtos
 
     $('.featured-item a').addClass('btn btn-dark stretch-link')
@@ -44,9 +45,178 @@ $('.navbar-toggler').click(function(){
     $('#navbarResponsive').toggle();
 });
 
+$('.nav-item').click(function(){
+    $('#navbarResponsive').hide();
+})
+
+/*
+ * Callback
+ * Entendendo ações que começam ao término de outra
+*/
+/* $('.featured-item:nth(1)')
+    .hide(500, function(){
+        // este é o callback
+        console.log($(this).find('h4').text() + ' esgotado')
+    })
+    .show(500, function(){
+        console.log($(this).find('h4').text() + ' em estoque')
+    }) */
+
+/*
+ * Animações
+*/
+/* const duracao = 1000
+$('.featured-item:nth(0)')
+.hide(duracao)
+.show(duracao)
+.fadeOut(duracao)
+    .fadeIn(duracao)
+    .toggle(duracao)
+    .toggle(duracao)
+    
+    $('#form-submit').on('click', function(e){
+        
+        e.preventDefault()
+        
+        if($('#email').val() != ''){
+            $('#email').animate({
+                opacity: "toggle",
+                top: "-50px"
+            }, duracao, function(){
+                console.log($(this).val())
+            })
+        
+        }
+        
+        
+    });
+    
+    */
+   /*
+   * Event listener .nav-modal-open 
+   */
+  
+  $('.nav-modal-open').on('click', function(e){
+      
+      e.preventDefault();
+      
+      let elem = $(this).attr('rel')
+      
+      $('.modal-body').html($('#' + elem).html())
+      $('.modal-header h5.modal-title').html($(this).text())
+      
+      let myModal = new bootstrap.Modal($('#modalId'))
+      
+      myModal.show()
+      
+    })
+    
+    function validate(elem){
+        if (elem.val() == ''){
+    
+            console.log('O campo de ' + elem.attr('name') + ' é obrigatório.')
+
+            elem.parent().find('.text-muted').show()
+    
+            elem.addClass('invalid')
+    
+            return false
+        } else {
+            elem.removeClass('invalid')
+            elem.parent().find('.text-muted').hide()
+        }
+    
+    }
+
+    $('body').on('blur', '#contato-nome', function(){
+        if ($(this).val().match('^[a-zA-Z]{3,16}$')) {
+            $(this).removeClass('invalid')
+
+            $(this).parent().find('.text-muted').hide()
+
+        } else {
+            $(this).parent().find('.text-muted').show()
+    
+            $(this).addClass('invalid')
+        }
+
+    })
+    
+    $('body').on('blur', '#contato-email', function(){
+        if ($(this).val().match('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$')){
+
+            $(this).removeClass('invalid')
+
+            $(this).parent().find('.text-muted').hide()
+
+        } else {
+            $(this).parent().find('.text-muted').show()
+    
+            $(this).addClass('invalid')
+        }
+    })
+    
+    $('body').on('submit', '.modal-body .form', function(e){
+
+        
+        const inputName = $('#contato-nome')
+        const inputEmail = $('#contato-email')
+        
+        if(inputEmail.hasClass('invalid') || inputName.hasClass('invalid')){
+            e.preventDefault();
+            console.log('Campos inválidos')
+            return false
+            
+        } else {
+            $(this).submit()
+        }
+
+    })
+
+    
+    
+    
+    $('#modalId').on('shown.bs.modal', function (){
+        $('#date').mask('00/00/0000');
+        $('#cep').mask('00000-000');
+        $('#phone').mask('00000-0000');
+        $('#cpf').mask('000.000.000-00');
+        
+    })
+
+    $('body').on('blur', '#date, #cep, #phone, #cpf', function(){
+        validate($(this))
+    })
+    
+    $('body').on('focus', '#date', function(){
+        $(this).datepicker({
+            closeText: "Fechar",
+            prevText: "Anterior",
+            nextText: "Próximo",
+            currentText: "Hoje",
+            monthNames: [ "Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho",
+            "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro" ],
+            monthNamesShort: [ "Jan", "Fev", "Mar", "Abr", "Mai", "Jun",
+            "Jul", "Ago", "Set", "Out", "Nov", "Dez" ],
+            dayNames: [
+                "Domingo",
+                "Segunda-feira",
+                "Terça-feira",
+                "Quarta-feira",
+                "Quinta-feira",
+                "Sexta-feira",
+                "Sábado"
+            ],dayNamesShort: [ "Dom", "Seg", "Ter", "Qua", "Qui", "Sex", "Sáb" ],
+            dayNamesMin: [ "Dom", "Seg", "Ter", "Qua", "Qui", "Sex", "Sáb" ],
+            weekHeader: "Sm",
+            dateFormat: "dd/mm/yy"
+        });
+        
+        
+    })
+
+    
+    
 })
     
-
-
-
 
--- jquery/vendor/owl/js/owl.js
+++ jquery/vendor/owl/js/owl.js
@@ -3446,7 +3446,6 @@
 	}
 
 	$('.owl-carousel').owlCarousel({
-		loop: true,
 		responsive: {
 			0:{
 				items:1,

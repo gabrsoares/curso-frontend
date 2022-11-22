d8a87c735150f9c64943fb5f5c8ef5e0b78b5a3f
Gabriel Soares <gabrsoares@outlook.com>
2022-11-22 15:13:34 -0300

    atualizando 2 modulo gulp

--- gulp/projeto/gulpfile.js
+++ gulp/projeto/gulpfile.js
@@ -1,13 +1,16 @@
+const { series, parallel } = require('gulp')
 const gulp = require('gulp')
 const concat = require('gulp-concat')
 const cssmin = require('gulp-cssmin')
 const rename = require('gulp-rename')
 const uglify = require('gulp-uglify')
 const image = require('gulp-image')
+const htmlmin = require('gulp-htmlmin')
+const babel = require('gulp-babel')
 
 function tarefasCSS(cb) {
 
-    return gulp.src([
+    gulp.src([
         './node_modules/bootstrap/dist/css/bootstrap.css',
         './vendor/owl/css/owl.css',
         './vendor/jquery-ui/jquery-ui.css',
@@ -18,22 +21,30 @@ function tarefasCSS(cb) {
         .pipe(rename({ suffix: '.min'})) // libs.min.css
         .pipe(gulp.dest('./dist/css'))
 
+     cb()
+
 }
 
-function tarefasJS(){
+function tarefasJS(callback){
 
-    return gulp.src([
+    gulp.src([
             './node_modules/jquery/dist/jquery.js',
             './node_modules/bootstrap/dist/js/bootstrap.js',
             './vendor/owl/js/owl.js',
             './vendor/jquery-mask/jquery.mask.js',
             './vendor/jquery-ui/jquery-ui.js',
             './src/js/custom.js' 
-        ])
+    ])
+    .pipe(babel({
+        comments: false,
+        presets: ['@babel/env']
+    }))
         .pipe(concat('scripts.js'))
         .pipe(uglify())
         .pipe(rename({ suffix: '.min'})) // libs.min.js
         .pipe(gulp.dest('./dist/js'))
+
+        return callback()
 
 }
 
@@ -55,6 +66,19 @@ function tarefasImagem() {
 
 }
 
+// POC - Proof of concept
+function tarefasHTML(callback){
+
+    gulp.src('./src/**/*.html')
+        .pipe(htmlmin({ collapseWhitespace: true }))
+        .pipe(gulp.dest('./dist'))
+
+    return callback()
+
+}
+
 exports.styles = tarefasCSS
 exports.scripts = tarefasJS
 exports.images = tarefasImagem
+
+exports.default = parallel( tarefasHTML, tarefasJS, tarefasCSS)
